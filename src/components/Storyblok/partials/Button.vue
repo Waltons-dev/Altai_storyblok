b20a7795a131fdbb96510bba30555228fc366d0c
Kirill <zahvatinkirill@mail.ru>
2024-06-21 21:47:12 +0300

    фича: создан элемент кнопки, с изменяемым содержимым

---
+++ src/components/Storyblok/partials/Button.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<script>
+
+</script>
+
+<template>
+  <button v-editable="blok">{{blok.text}}</button>
+</template>
+
+
+
+<style lang="scss">
+.calendar__btn{
+  cursor: pointer;
+  max-width: 95px;
+  height: 60px;
+  background-color: #EF4444;
+  border: 3px solid #EF4444;
+  border-radius: 12px;
+  color: #F9FAFB;
+}
+.calendar__btn_booked{
+  background-color: #111827;
+}
+
+.article__tag{
+  background-color: #EF4444AB;
+  border-radius: 16px;
+  font-family: "SoyuzGroteskBold", sans-serif;
+  color: white;
+  font-size: 24px;
+  line-height: 28.8px;
+  padding: 4px 20px;
+}
+</style>
