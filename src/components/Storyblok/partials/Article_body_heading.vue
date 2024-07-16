4f4996e6cd2c6112c92a116d503928ca00e0cde0
Kirill <zahvatinkirill@mail.ru>
2024-07-17 01:49:33 +0300

    feat: создан компонент заголовка внутри тела статьи

---
+++ src/components/Storyblok/partials/Article_body_heading.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<template>
+  <h3 v-editable="blok">{{ blok.head }}</h3>
+</template>
+
+<style scoped lang="scss">
+.article__text-heading{
+  font-family: "SoyuzGroteskBold", sans-serif;
+  font-size: 26px;
+  line-height: 32px;
+  margin-bottom: 0;
+}
+</style>
