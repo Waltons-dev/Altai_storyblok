e549e2a2189e16cb7faba4de2158c7bb569979ca
Kirill <zahvatinkirill@mail.ru>
2024-07-17 01:47:57 +0300

    fix: исправлены баги

--- src/components/Storyblok/components/Article_Text_Img.vue
+++ src/components/Storyblok/components/Article_Text_Img.vue
@@ -1,36 +1,26 @@
 <script setup>
-import { computed } from "vue";
-import {renderRichText} from "@storyblok/vue";
-const props = defineProps({ blok: Object })
-const articleContent = computed(() => renderRichText(props.blok.text));
-</script>
+import {StoryblokComponent} from "@storyblok/vue";
 
-<script>
-//костыль чтобы убрать <p>, так как сториблок зачем-то добавляет пустой <p> при добавлении richtext, а это кринж
-export default {
-  mounted() {
-    // const RichTextParent = document.querySelector(".article__main-text");
-    // const articleDescription=document.querySelector(".article__body-element");
-    // const richText = RichTextParent.firstChild;
-    // RichTextParent.firstChild.classList.add("article__main-text");
-    // RichTextParent.remove()
-    // articleDescription.prepend(richText)
-  }
-}
+const props = defineProps({ blok: Object })
 </script>
 
 <template>
-  <div class="article__body-element">
-    <p class="article__main-text" v-html="articleContent"></p>
-
-    <img class="article__body-text-image" :src=blok.img :alt=blok.alternative_text>
+  <div>
+    <div class="article__text-image">
+      <div class="article__body-element-text">
+        <StoryblokComponent class="article__text-heading" v-editable="blok" v-for="inblok in blok.columns.slice(0,1)" :key="inblok._uid" :blok="inblok" />
+        <StoryblokComponent class="article__body-text" v-editable="blok" v-for="inblok in blok.columns.slice(1)" :key="inblok._uid" :blok="inblok" />
+      </div>
+      <img loading="lazy" class="article__body-text-image" :src=blok.img :alt=blok.alternative_text>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-.article__body-element{
+.article__text-image{
   display: flex;
-  gap: 151px;
+  align-items: center;
+  gap: 10%;
 }
 
 .article__main-text{
@@ -40,8 +30,9 @@ export default {
 }
 .article__body-text-image{
   object-fit: cover;
-  width: 100%;
+  width: 45%;
   max-height: 600px;
   border-radius: 16px;
 }
+
 </style>
