a5a56d1a95b4ba9cc4a57af264da6d0544ab294e
Kirill <zahvatinkirill@mail.ru>
2024-07-20 02:09:52 +0300

    feat: удалены компоненты, чей функционал впредь ненужен из-за пересмотра работы компонентов Article_Text_Img и Article_body_heading

--- src/main.js
+++ src/main.js
@@ -10,12 +10,11 @@ import Article_Text_Img from "./components/Storyblok/components/Article_Text_Img
 import Article_Info from "./components/Storyblok/components/Article_Info.vue";
 import Article_HeroActivities from "./components/Storyblok/components/Article_HeroActivities.vue";
 import Article_Hero from "./components/Storyblok/sections/Article_Hero.vue";
+import Article_elem from "./components/Storyblok/sections/Article_elem.vue";
 import Article_Body from "./components/Storyblok/sections/Article_Body.vue";
 import Article_Image from "./components/Storyblok/partials/Article_Image.vue";
 import Button from "./components/Storyblok/partials/Button.vue";
 import Article_body_heading from "./components/Storyblok/partials/Article_body_heading.vue";
-import Article_RightText_Img from "./components/Storyblok/components/Article_RightText_Img.vue";
-import Article_body_FormularHeading from "./components/Storyblok/partials/Article_body_FormularHeading.vue";
 
 
 
@@ -28,6 +27,7 @@ app.use(StoryblokVue, {
 });
 app.component("Headline", Headline);
 app.component("Article_Hero", Article_Hero);
+app.component("Article_elem", Article_elem);
 app.component("Article_Body", Article_Body);
 app.component("Page", Page);
 app.component("Textarea", Textarea);
@@ -38,7 +38,5 @@ app.component("Article_HeroActivities", Article_HeroActivities);
 app.component("Button", Button);
 app.component("Article_Image", Article_Image);
 app.component("Article_body_heading",Article_body_heading)
-app.component("Article_RightText_Img",Article_RightText_Img)
-app.component("Article_body_FormularHeading",Article_body_FormularHeading)
 
 app.use(head).mount("#app");
